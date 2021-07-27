4e014f9e969cee1ef03193c4a87ac32a16df41a6
EssiJunior <nessipjunior@gmail.com>
2021-07-27 01:45:16 +0100

    Executable du calcul de MGP

---
+++ convertiseurMGP.py
@@ -0,0 +1,6 @@
+#Coding :utf-8
+
+print("+--------------------+ Convertiseur de MGP(/4) en Moyenne(/20) +-------------------------+")
+MGP = float(input("Entrez votre MGP: ")) 
+Moyenne = MGP*5
+print(f"Vous avez eu: {Moyenne:.2f} /20  ")
